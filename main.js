1ce758a596ed2c4410c58476aaca7737cef24879
Afnan MT <afnanxmt@gmail.com>
2020-02-05 09:07:46 +0300

    files modified

--- main.js
+++ main.js
@@ -13,58 +13,80 @@ let winner=false;
 
 
 
+function reset() {
+  
+$('.place').removeClass('O')
+$('.place').removeClass('X')
+$('.place').html(' ')
+  console.log("reset")
+count=0; 
+playerX=[]; // store the score 
+playerO=[];
+winner=false;
+
+$('h2').show();
+$('h2').html('Turns.. ')
+$('h3').hide(); 
+console.log(count)
+ // location.reload();
+ playTurn(event)
+}
+$("#replay").on('click', reset);
+
+
 const gamePlay = function () 
 { 
-    // chick WHo win .. 
-    console.log("checking for winner...")
-   
+   playerO; 
+   playerX;
 
     // wining conditions for the Game .. using the color attr. to calculate the result.
     // row
-    const playerXrow1 = $("#O-1.pink , #O-2.pink , #O-3.pink").length === 3
-    const playerXrow2 = $("#T-1.pink , #T-2.pink , #T-3.pink").length === 3
-    const playerXrow3 = $("#Th-1.pink , #Th-2.pink , #Th-3.pink").length === 3
+    const playerXrow1 = $("#O-1.X , #O-2.X , #O-3.X").length === 3
+    const playerXrow2 = $("#T-1.X , #T-2.X , #T-3.X").length === 3
+    const playerXrow3 = $("#Th-1.X , #Th-2.X , #Th-3.X").length === 3
 
     //col 
 
-    const playerXcol1 = $("#O-1.pink , #T-1.pink , #Th-1.pink").length === 3
-    const playerXcol2 = $("#O-2.pink , #T-2.pink , #Th-2.pink").length === 3
-    const playerXcol3 = $("#O-3.pink , #T-3.pink , #Th-3.pink").length === 3
+    const playerXcol1 = $("#O-1.X , #T-1.X , #Th-1.X").length === 3
+    const playerXcol2 = $("#O-2.X , #T-2.X , #Th-2.X").length === 3
+    const playerXcol3 = $("#O-3.X , #T-3.X , #Th-3.X").length === 3
     // diagonal 
-    const playerXdia1 = $("#O-1.pink , #T-2.pink , #Th-3.pink").length === 3
-    const playerXdia2 = $("#O-3.pink , #T-2.pink , #Th-1.pink").length === 3
+    const playerXdia1 = $("#O-1.X , #T-2.X , #Th-3.X").length === 3
+    const playerXdia2 = $("#O-3.X , #T-2.X , #Th-1.X").length === 3
 
     // row 
-    const playerOrow1 = $("#O-1.red , #O-2.red , #O-3.red").length === 3
-    const playerOrow2 = $("#T-1.red , #T-2.red , #T-3.red").length === 3
-    const playerOrow3 = $("#Th-1.red , #Th-2.red , #Th-3.red").length === 3
+    const playerOrow1 = $("#O-1.O , #O-2.O , #O-3.O").length === 3
+    const playerOrow2 = $("#T-1.O , #T-2.O , #T-3.O").length === 3
+    const playerOrow3 = $("#Th-1.O , #Th-2.O , #Th-3.O").length === 3
 
     //col 
 
-    const playerOcol1 = $("#O-1.red , #T-1.red , #Th-1.red").length === 3
-    const playerOcol2 = $("#O-2.red , #T-2.red , #Th-2.red").length === 3
-    const playerOcol3 = $("#O-3.red , #T-3.red , #Th-3.red").length === 3
+    const playerOcol1 = $("#O-1.O , #T-1.O , #Th-1.O").length === 3
+    const playerOcol2 = $("#O-2.O , #T-2.O , #Th-2.O").length === 3
+    const playerOcol3 = $("#O-3.O , #T-3.O , #Th-3.O").length === 3
     // diagonal 
-    const playerOdia1 = $("#O-1.red , #T-2.red , #Th-3.red").length === 3
-    const playerOdia2 = $("#O-3.red , #T-2.red , #Th-1.red").length === 3
+    const playerOdia1 = $("#O-1.O , #T-2.O , #Th-3.O").length === 3
+    const playerOdia2 = $("#O-3.O , #T-2.O , #Th-1.O").length === 3
 
-
-    let xScoreCount = getOCount()
-    let oScoreCount = getXCount()
-    
+    winX=( playerXrow1 || playerXrow2 || playerXrow3 || playerXcol1 || playerXcol2 || playerXcol3 || playerXdia1 || playerXdia2 )
+    winO=( playerOrow1 || playerOrow2 || playerOrow3 || playerOcol1 || playerOcol2 || playerOcol3 || playerOdia1 || playerOdia2)
     
 
-  if ( xScoreCount === playerXrow1 || playerXrow2 || playerXrow3 || playerXcol1 || playerXcol2 || playerXcol3 || playerXdia1 || playerXdia2 )
+  if ( winX )
   { 
+    
     console.log('X player win')
     $('h2').hide();
+    $('h3').show();
     $('h3').text('X player win'); 
     return winner = true;
   }
-  if ( oScoreCount === playerOrow1 || playerOrow2 || playerOrow3 || playerOcol1 || playerOcol2 || playerOcol3 || playerOdia1 || playerOdia2 )
+  if ( winO )
   { 
+   
     console.log('O player win')
     $('h2').hide();
+    $('h3').show();
     $('h3').text('O player win'); 
     return winner = true;
   }
@@ -73,36 +95,17 @@ const gamePlay = function ()
     }
   }
 
-
-  function reset() {
-      /*
-    console.log("reset")
-    $(".place").removeClass("pink red")
-    playerX=[]; // store the score 
-    playerO=[];
-    */
-    
-    location.reload();
- }
-
-
-
- $("#replay").on('click', reset);
-
-
 function playTurn(event) {
     $(".place").click(function(event) {
-      console.log("0. this is: ", this)
-      console.log("0. you clicked: ")//thingClicked)
-       
-        if(count%2===0 && count<9 )  
+       console.log(count)
+        if(count%2===0 && count<= 9 )  
         { 
-            $(this).css("background-color", "pink")
-            $(this).html("X")
-            $(this).addClass("pink") 
+           // $(this).css("background-color", "pink")
+          $(this).removeClass("O")
+           $(this).html("X")
+            $(this).addClass("X") 
             playerX.push(this);
             $('h2').text('O turn');  
-
             if ( playGame() === true)
             { 
                 count =10;
@@ -110,27 +113,29 @@ function playTurn(event) {
 
         }
 
-        if(count%2 !==0 && count<9) 
+        if(count%2!==0 && count<= 9 )
         { 
-            $(this).css("background-color", "red")
+          $(this).removeClass("X") // just to make sure there is no bugs..
+            //$(this).css("background-color", "rosybrown")
             $(this).html("O")
-            // push the O value in the score index. 
-            $(this).addClass("red") 
+            // push the O value in the score index.
+            $(this).addClass("O")
             playerO.push(this);
             $('h2').text('X turn');
             if ( playGame() === true)
             { 
                 count =10;
             }
-
         }
         $( this ).off( event )
-        
+        playGame()
     count++;
-    playGame()
+    
+    
     })
     
   }
+  
   playTurn(event)
    
  
@@ -155,15 +160,15 @@ function getXCount() {
     if (count === 9 ) {
         console.log('Tie game!')
         $('h2').hide();
+        $('h3').show();
         $('h3').text('Tie !'); 
-        return winnewinner = true;
+        return winner = true;
       } 
     else {
-      console.log('no winner yet...')
       gamePlay(); 
-
     }
 }
+
 
 
 
